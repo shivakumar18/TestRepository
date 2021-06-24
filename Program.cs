477532d8255ab77b00308dc8f58df3f3c9ec1a03
shivakumar18 <shivakumar.kumar18@gmail.com>
2021-06-24 11:57:48 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleVSCodeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! From Mysore !");
+        }
+    }
+}
